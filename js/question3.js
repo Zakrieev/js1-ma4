9684a09a0469a4bdfd4db6e7b54fec19765a4cf4
Zakrieev <57872517+Zakrieev@users.noreply.github.com>
2020-02-16 22:23:29 +0100

    Create question3.js

---
+++ js/question3.js
@@ -0,0 +1,24 @@
+const URL = "https://api.rawg.io/api/games/4200";
+
+fetch(URL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    createGameDetails(json);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+function createGameDetails(json) {
+
+    const heading = document.querySelector("h1");
+    heading.innerHTML = json.name;
+
+    const image = document.querySelector(".image");
+    image.style.backgroundImage = `url('${json.background_image}')`;
+
+    const description = document.querySelector(".description");
+    description.innerHTML = json.description;
+}
